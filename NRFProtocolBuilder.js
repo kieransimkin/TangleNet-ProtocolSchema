1b04424f06dac8d80b77342e96c63e2115735a12
Kieran Simkin <kieran@slinq.com>
2016-04-09 11:25:52 +0100

    changes

--- NRFProtocolBuilder.js
+++ NRFProtocolBuilder.js
@@ -9,18 +9,19 @@ var xml = fs.readFileSync('NRFProtocol.xml', {encoding: 'utf-8'});
 //request.post({url:'https://validator.w3.org/nu/?parser=xml&out=xml&laxtype=yes',headers: {'Content-Type':'text/xml; charset=utf-8'},body: xml},function(err,httpResponse,body){ console.log(body); });
 //request.post({url:'https://validator.w3.org/nu/?schema=http://slinq.com/NRFProtocol.xsd&parser=xml&out=xml&laxtype=yes',headers: {'Content-Type':'text/xml; charset=utf-8'},body: xml},function(err,httpResponse,body){ console.log(body); });
 
-request.post({url:'https://validator.w3.org/nu/?out=xml&schema=http://raw.githubusercontent.com/kieransimkin/TangleNet-ProtocolSchema/master/NRFProtocol.xsd&laxtype=yes',headers: {'Content-Type':'text/xml; charset=utf-8'},body: xml},function(err,httpResponse,body){ console.log(body); });
-//request.post({url:'https://validator.w3.org/nu/?parser=xml&out=json&schema=http://slinq.com/NRFProtocol.xsd&laxtype=yes',headers: {'Content-Type':'text/xml; charset=utf-8'},body: xml},function(err,httpResponse,body){ console.log(body); });
+//request.post({url:'https://validator.w3.org/nu/?out=xml&schema=http://raw.githubusercontent.com/kieransimkin/TangleNet-ProtocolSchema/master/NRFProtocol.xsd&laxtype=yes',headers: {'Content-Type':'text/xml; charset=utf-8'},body: xml},function(err,httpResponse,body){ console.log(body); });
+request.post({url:'https://validator.w3.org/nu/?parser=xml&out=xml&schema=http://raw.githubusercontent.com/kieransimkin/TangleNet-ProtocolSchema/master/NRFProtocol.xsd&laxtype=yes',headers: {'Content-Type':'text/xml; charset=utf-8'},body: xml},function(err,httpResponse,body){ console.log(body); });
+request.post({url:'https://validator.w3.org/nu/?parser=xmldtd&out=json&laxtype=yes',headers: {'Content-Type':'text/xml; charset=utf-8'},body: xml},function(err,httpResponse,body){ console.log(body); });
 
+var parser = new xml4js.Parser({downloadSchemas:true});
 /*
-var parser = new xml4js.Parser({ downloadSchemas: false,xmlns: true});
 parser.addSchema('http://dealing.ninja/NRFProtocol', schema, function (err, importsAndIncludes) {
-	console.log(err);
+	console.log(err);a
+	*/
     // importsAndIncludes contains schemas to be added as well to satisfy all imports and includes found in schema.xsd
     parser.parseString(xml, function (err, result) {
 	    console.log(err);
 	    console.log(result);
         console.log(util.inspect(result, false, null));
-    });
-}); // */
+    }); // */
 
