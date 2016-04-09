0595e6fb3cf067549ca5e13afec519327ca3b074
Kieran Simkin <kieran@slinq.com>
2016-04-09 02:22:57 +0100

    changes

---
+++ NRFProtocolBuilder.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+var fs = require('fs');
+var util = require('util');
+var xml4js = require('xml4js');
+
+var schema = fs.readFileSync('NRFProtocol.xsd', {encoding: 'utf-8'});
+var xml = fs.readFileSync('NRFProtocol.xml', {encoding: 'utf-8'});
+var parser = new xml4js.Parser({ downloadSchemas: false,xmlns: false});
+parser.addSchema('', schema, function (err, importsAndIncludes) {
+	console.log(err);
+    // importsAndIncludes contains schemas to be added as well to satisfy all imports and includes found in schema.xsd 
+    parser.parseString(xml, function (err, result) {
+        console.log(util.inspect(result, false, null));
+    });
+});
+
