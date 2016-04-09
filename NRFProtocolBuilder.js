2e93a85161f01b535131d6e18521af44812032a6
Kieran Simkin <kieran@slinq.com>
2016-04-09 10:03:01 +0100

    changes

--- NRFProtocolBuilder.js
+++ NRFProtocolBuilder.js
@@ -15,14 +15,14 @@ request.post({url:'https://validator.w3.org/nu/?out=xml&schema=http://raw.github
 
 //request.post({url:'https://validator.w3.org/nu/?parser=xml&out=json&schema=http://raw.githubusercontent.com/kieransimkin/TangleNet-ProtocolSchema/995b181adb07d6f9ce6692ca537b2d8bbb87d931/NRFProtocol.xsd&laxtype=yes',headers: {'Content-Type':'text/xml; charset=utf-8'},body: xml},function(err,httpResponse,body){ console.log(body); });
 //request.post({url:'https://validator.w3.org/nu/?parser=xml&out=json&laxtype=yes',headers: {'Content-Type':'text/xml; charset=utf-8'},body: xml},function(err,httpResponse,body){ console.log(body); });
-var parser = new xml4js.Parser({ downloadSchemas: false,xmlns: false});
+var parser = new xml4js.Parser({ downloadSchemas: false,xmlns: true});
 parser.addSchema('http://dealing.ninja/NRFProtocol', schema, function (err, importsAndIncludes) {
 	console.log(err);
     // importsAndIncludes contains schemas to be added as well to satisfy all imports and includes found in schema.xsd
-    parser.parseString(xml, function (err, result) {
+  /*  parser.parseString(xml, function (err, result) {
 	    console.log(err);
 	    console.log(result);
         console.log(util.inspect(result, false, null));
-    });
+    });*/
 });
 
