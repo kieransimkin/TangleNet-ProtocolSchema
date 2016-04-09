995b181adb07d6f9ce6692ca537b2d8bbb87d931
Kieran Simkin <kieran@slinq.com>
2016-04-09 05:41:19 +0100

    changes

--- NRFProtocolBuilder.js
+++ NRFProtocolBuilder.js
@@ -8,7 +8,8 @@ var xml = fs.readFileSync('NRFProtocol.xml', {encoding: 'utf-8'});
 //request.post({url:'https://validator.w3.org/nu/?out=json',headers: {'Content-Type':'text/xml; charset=utf-8'},body: schema},function(err,httpResponse,body){ console.log(body); });
 //request.post({url:'https://validator.w3.org/nu/?parser=xml&out=xml&laxtype=yes',headers: {'Content-Type':'text/xml; charset=utf-8'},body: xml},function(err,httpResponse,body){ console.log(body); });
 //request.post({url:'https://validator.w3.org/nu/?schema=http://slinq.com/NRFProtocol.xsd&parser=xml&out=xml&laxtype=yes',headers: {'Content-Type':'text/xml; charset=utf-8'},body: xml},function(err,httpResponse,body){ console.log(body); });
-request.post({url:'https://validator.w3.org/nu/?parser=xml&out=xml&schema=http://raw.githubusercontent.com/kieransimkin/TangleNet-ProtocolSchema/master/NRFProtocol.xsd&laxtype=yes',headers: {'Content-Type':'text/xml; charset=utf-8'},body: xml},function(err,httpResponse,body){ console.log(body); });
+request.post({url:'https://validator.w3.org/nu/?parser=xml&out=json&schema=http://raw.githubusercontent.com/kieransimkin/TangleNet-ProtocolSchema/master/NRFProtocol.xsd&laxtype=yes',headers: {'Content-Type':'text/xml; charset=utf-8'},body: xml},function(err,httpResponse,body){ console.log(body); });
+//request.post({url:'https://validator.w3.org/nu/?parser=xml&out=json&laxtype=yes',headers: {'Content-Type':'text/xml; charset=utf-8'},body: xml},function(err,httpResponse,body){ console.log(body); });
 /*
 var parser = new xml4js.Parser({ downloadSchemas: false,xmlns: false});
 parser.addSchema('nrf', schema, function (err, importsAndIncludes) {
